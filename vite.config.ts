0272909efdf74328f05871c7bdfebfa0ad308854
Jaylistisnotavailable <168703458+Jaylistisnotavailable@users.noreply.github.com>
2026-08-17 14:41:32 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -212,7 +212,7 @@ const plugins = [
 ];
 
 export default defineConfig({
-  base: '/Pdf-Editor/',
+  base: '/',
   plugins,
   optimizeDeps: {
     include: ['pdfjs-dist'],
@@ -227,7 +227,7 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
